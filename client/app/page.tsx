f6258662d4d06dcec8f0c03b13b71e2054930600
Nikhil <nikhil.4002.50.82@gmail.com>
2026-04-03 21:00:12 +0530

    setup next js

---
+++ client/app/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='bg-white'>
+      <h1 className='text-3xl text-red-600'>hello world</h1>
+    </div>
+  )
+}
+
+export default page
