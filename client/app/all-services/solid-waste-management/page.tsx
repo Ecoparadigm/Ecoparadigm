48773d25e530657e3fc00b761d788f2b0d993149
Nikhil <nikhil.4002.50.82@gmail.com>
2026-04-13 18:28:44 +0530

    removed comments

---
+++ client/app/all-services/solid-waste-management/page.tsx
@@ -0,0 +1,8 @@
+export default function SolidWasteManagement() {
+  return (
+    <div>
+      <h1>Solid Waste Management</h1>
+      <p>Information about solid waste management services.</p>
+    </div>
+  );
+}
