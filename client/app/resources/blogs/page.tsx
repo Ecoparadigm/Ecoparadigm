0f5c7771cabd0b7495864fe5eaed9f5289daf8b4
Nikhil <nikhil.4002.50.82@gmail.com>
2026-04-27 12:01:53 +0530

    completed frontend

--- client/app/resources/blogs/page.tsx
+++ client/app/resources/blogs/page.tsx
@@ -1,9 +1,11 @@
 import KnowledgeCenter from "@/components/blogs/KnowledgeCenter";
+import CTASection from "@/components/blogs/CTASection";
 
 export default function BlogsPage() {
   return (
     <>
       <KnowledgeCenter />
+      <CTASection />
     </>
   );
 }
